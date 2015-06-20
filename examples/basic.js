0cce61e840b26af4409c4211d1480edb27cdf3b7
Wim <wim@sitebase.be>
2015-06-20 15:30:18 +0200

    update example address

--- examples/basic.js
+++ examples/basic.js
@@ -1,6 +1,6 @@
 var Piko = require('../src/index');
 
-var pv = new Piko({ host: 'http://piko.lan' });
+var pv = new Piko({ host: 'http://192.168.1.127' });
 pv.fetch(function( result ) {
 	console.log(result);
 });
